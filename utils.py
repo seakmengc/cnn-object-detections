debf7d164ea76df65c1d793a1f7ec8244fabed2f
SeakmengChheang <c.seakmeng0603@gmail.com>
2021-10-04 02:14:24 +0700

    added support ndarray to iou fn

--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ import torch
 
 
 def _midpoint_to_corners(box):
-    return [
-        box[0] - box[2] / 2,  # x1
-        box[1] - box[3] / 2,  # y1
-        box[0] + box[2] / 2,  # x2
-        box[1] + box[3] / 2,  # y2
-    ]
+    return torch.Tensor([
+        box[..., 0:1] - box[..., 2:3] / 2,  # x1
+        box[..., 1:2] - box[..., 3:4] / 2,  # y1
+        box[..., 0:1] + box[..., 2:3] / 2,  # x2
+        box[..., 1:2] + box[..., 3:4] / 2,  # y2
+    ])
 
 
 def intersection_over_union(box_pred, ground_box, format):
@@ -24,21 +24,15 @@ def intersection_over_union(box_pred, ground_box, format):
         box1 = box_pred
         box2 = ground_box
 
-    x1, y1 = torch.max(box1[0], box1[0]), torch.max(box1[1], box2[1])
-    x2, y2 = torch.min(box1[2], box1[2]), torch.min(box1[3], box2[3])
+    x1, y1 = torch.max(box1[..., 0:1], box1[..., 0:1]), torch.max(
+        box1[..., 1:2], box2[..., 1:2])
+    x2, y2 = torch.min(box1[..., 2:3], box1[..., 2:3]), torch.min(
+        box1[..., 3:4], box2[..., 3:4])
 
     intersection_area = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
-    box1_area = abs((box1[2] - box1[0]) * (box1[1] - box1[3]))
-    box2_area = abs((box2[2] - box2[0]) * (box2[1] - box2[3]))
+    box1_area = abs((box1[..., 2:3] - box1[..., 0:1])
+                    * (box1[..., 1:2] - box1[..., 3:4]))
+    box2_area = abs((box2[..., 2:3] - box2[..., 0:1])
+                    * (box2[..., 1:2] - box2[..., 3:4]))
 
-    return (intersection_area / (box1_area + box2_area - intersection_area + 1e-7)).item()
-
-
-if __name__ == '__main__':
-    print(
-        intersection_over_union(
-            torch.Tensor([1, 1, 2, 2]),
-            torch.Tensor([1.5, 1.5, 2.5, 2.5]),
-            'corners'
-        )
-    )
+    return intersection_area / (box1_area + box2_area - intersection_area + 1e-7)
--- utils.test.py
+++ utils.test.py
@@ -6,40 +6,40 @@ from utils import intersection_over_union
 
 class TestIOUFunction(unittest.TestCase):
     def test_corners_format(self):
-        box_pred = torch.Tensor([1, 1, 2, 2])
-        ground_box = torch.Tensor([1.5, 1.5, 2.5, 2.5])
+        box_pred = torch.Tensor([[1, 1, 2, 2]])
+        ground_box = torch.Tensor([[1.5, 1.5, 2.5, 2.5]])
         format = 'corners'
 
         res = intersection_over_union(box_pred, ground_box, format)
 
-        self.assertAlmostEqual(res, 0.3333, 4)
+        self.assertAlmostEqual(res[0].item(), 0.3333, 4)
 
     def test_corners_format_with_full_overlap(self):
-        box_pred = torch.Tensor([1, 1, 2, 2])
-        ground_box = torch.Tensor([1, 1, 2, 2])
+        box_pred = torch.Tensor([[1, 1, 2, 2]])
+        ground_box = torch.Tensor([[1, 1, 2, 2]])
         format = 'corners'
 
         res = intersection_over_union(box_pred, ground_box, format)
 
-        self.assertAlmostEqual(res, 1, 4)
+        self.assertAlmostEqual(res[0].item(), 1, 4)
 
     def test_midpoint_format(self):
-        box_pred = torch.Tensor([1.5, 1.5, 1, 1])
-        ground_box = torch.Tensor([2, 2, 1, 1])
+        box_pred = torch.Tensor([[1.5, 1.5, 1, 1]])
+        ground_box = torch.Tensor([[2, 2, 1, 1]])
         format = 'midpoint'
 
         res = intersection_over_union(box_pred, ground_box, format)
 
-        self.assertAlmostEqual(res, 0.3333, 4)
+        self.assertAlmostEqual(res[0].item(), 0.3333, 4)
 
     def test_midpoint_format_with_full_overlap(self):
-        box_pred = torch.Tensor([1.5, 1.5, 1, 1])
-        ground_box = torch.Tensor([1.5, 1.5, 1, 1])
+        box_pred = torch.Tensor([[1.5, 1.5, 1, 1]])
+        ground_box = torch.Tensor([[1.5, 1.5, 1, 1]])
         format = 'midpoint'
 
         res = intersection_over_union(box_pred, ground_box, format)
 
-        self.assertAlmostEqual(res, 1, 4)
+        self.assertAlmostEqual(res[0].item(), 1, 4)
 
 
 if __name__ == '__main__':
