c8802cbcb8e44bc3b8ad21166bf285ee9c2df285
SeakmengChheang <c.seakmeng0603@gmail.com>
2021-10-05 23:05:59 +0700

    added non_max suppresion fn

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def _midpoint_to_corners(box):
@@ -10,19 +11,19 @@ def _midpoint_to_corners(box):
     ])
 
 
-def intersection_over_union(box_pred, ground_box, format):
+def intersection_over_union(box_preds, ground_boxes, box_format):
     """
         box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
     """
 
     # convert midpoint box to corners
-    if format == 'midpoint':
-        box1 = _midpoint_to_corners(box_pred)
-        box2 = _midpoint_to_corners(ground_box)
+    if box_format == 'midpoint':
+        box1 = _midpoint_to_corners(box_preds)
+        box2 = _midpoint_to_corners(ground_boxes)
 
-    if format == 'corners':
-        box1 = box_pred
-        box2 = ground_box
+    if box_format == 'corners':
+        box1 = box_preds
+        box2 = ground_boxes
 
     x1, y1 = torch.max(box1[..., 0:1], box1[..., 0:1]), torch.max(
         box1[..., 1:2], box2[..., 1:2])
@@ -36,3 +37,54 @@ def intersection_over_union(box_pred, ground_box, format):
                     * (box2[..., 1:2] - box2[..., 3:4]))
 
     return intersection_area / (box1_area + box2_area - intersection_area + 1e-7)
+
+
+def non_max_suppresion(
+    bboxes,
+    iou_threshold=0.1,
+    threshold=0.5,
+    box_format='corners'
+):
+    """
+        bboxes (tensor): [[class, pred, x1, y1, x2, y2]]
+    """
+
+    bboxes = [
+        bbox if box_format == 'corners' else _midpoint_to_corners(bbox)
+        for bbox in bboxes if bbox[1] >= threshold
+    ]
+
+    # sort by pred in desc order
+    bboxes = sorted(bboxes,
+                    key=lambda x: x[1],  # by pred score
+                    reverse=True)
+
+    rtn_bboxes = []
+
+    while bboxes:
+        curr = bboxes.pop(0)
+
+        bboxes = [
+            bbox
+            for bbox in bboxes
+            if bbox[0] != curr[0]  # class
+            or intersection_over_union(bbox[2:].unsqueeze(0), curr[2:].unsqueeze(0), box_format=box_format)[0].item() <= iou_threshold
+        ]
+
+        rtn_bboxes.append(curr.tolist())
+
+    return torch.Tensor(
+        np.asarray(rtn_bboxes)
+    )
+
+
+if __name__ == '__main__':
+    print(non_max_suppresion(
+        torch.Tensor([
+            [1, 0.9, 1, 1, 2, 2],
+            [1, 0.8, 1, 1, 2, 2],
+            [1, 0.8, 1.90, 1.90, 3, 3],
+            [2, 0.9, 1, 1, 2, 2],
+            [2, 0.5, 1, 1, 2, 2],
+        ])
+    ))
--- utils.test.py
+++ utils.test.py
@@ -1,45 +1,101 @@
 import torch
 import unittest
 
-from utils import intersection_over_union
+from utils import intersection_over_union, non_max_suppresion
 
 
 class TestIOUFunction(unittest.TestCase):
     def test_corners_format(self):
         box_pred = torch.Tensor([[1, 1, 2, 2]])
         ground_box = torch.Tensor([[1.5, 1.5, 2.5, 2.5]])
-        format = 'corners'
+        box_format = 'corners'
 
-        res = intersection_over_union(box_pred, ground_box, format)
+        res = intersection_over_union(box_pred, ground_box, box_format)
 
         self.assertAlmostEqual(res[0].item(), 0.3333, 4)
 
     def test_corners_format_with_full_overlap(self):
         box_pred = torch.Tensor([[1, 1, 2, 2]])
         ground_box = torch.Tensor([[1, 1, 2, 2]])
-        format = 'corners'
+        box_format = 'corners'
 
-        res = intersection_over_union(box_pred, ground_box, format)
+        res = intersection_over_union(box_pred, ground_box, box_format)
 
         self.assertAlmostEqual(res[0].item(), 1, 4)
 
     def test_midpoint_format(self):
         box_pred = torch.Tensor([[1.5, 1.5, 1, 1]])
         ground_box = torch.Tensor([[2, 2, 1, 1]])
-        format = 'midpoint'
+        box_format = 'midpoint'
 
-        res = intersection_over_union(box_pred, ground_box, format)
+        res = intersection_over_union(box_pred, ground_box, box_format)
 
         self.assertAlmostEqual(res[0].item(), 0.3333, 4)
 
     def test_midpoint_format_with_full_overlap(self):
         box_pred = torch.Tensor([[1.5, 1.5, 1, 1]])
         ground_box = torch.Tensor([[1.5, 1.5, 1, 1]])
-        format = 'midpoint'
+        box_format = 'midpoint'
 
-        res = intersection_over_union(box_pred, ground_box, format)
+        res = intersection_over_union(box_pred, ground_box, box_format)
 
         self.assertAlmostEqual(res[0].item(), 1, 4)
+
+
+class TestNMSFunction(unittest.TestCase):
+    def test_it_should_remove_under_threshold(self):
+        bboxes = torch.Tensor([
+            [1, 0.3, 1, 1, 2, 2]
+        ])
+
+        res = non_max_suppresion(bboxes=bboxes, threshold=0.5)
+
+        self.assertEqual(len(res), 0)
+
+    def test_it_should_work_with_midpoint(self):
+        bboxes = torch.Tensor([
+            [1, 0.3, 1, 1, 2, 2]
+        ])
+
+        res = non_max_suppresion(
+            bboxes=bboxes, threshold=0.5, box_format='midpoint')
+
+        self.assertEqual(len(res), 0)
+
+    def test_it_should_return_one_given_one_input(self):
+        bboxes = torch.Tensor([
+            [1, 0.6, 1, 1, 2, 2]
+        ])
+
+        res = non_max_suppresion(bboxes=bboxes)
+
+        self.assertEqual(len(res), 1)
+        self.assertListEqual(res.tolist(), bboxes.tolist())
+
+    def test_it_should_suppress(self):
+        bboxes = torch.Tensor([
+            [1, 0.9, 1, 1, 2, 2],
+            [1, 0.8, 1, 1, 2, 2],
+            [1, 0.8, 1.90, 1.90, 3, 3],
+            [2, 0.9, 1, 1, 2, 2],
+            [2, 0.5, 1, 1, 2, 2],
+            [2, 0.4, 1, 1, 2, 2],
+            [3, 0.4, 1, 1, 2, 2],
+        ])
+
+        res = non_max_suppresion(bboxes, threshold=0.5)
+
+        expected = torch.Tensor([
+            [1, 0.9, 1, 1, 2, 2],
+            [1, 0.8, 1.90, 1.90, 3, 3],
+            [2, 0.9, 1, 1, 2, 2],
+        ])
+
+        self.assertEqual(len(res), len(expected))
+        self.assertListEqual(
+            sorted(res.tolist(), key=lambda x: x[0]),
+            sorted(expected.tolist(), key=lambda x: x[0]),
+        )
 
 
 if __name__ == '__main__':
